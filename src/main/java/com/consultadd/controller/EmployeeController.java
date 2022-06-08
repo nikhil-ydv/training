6481b0f395722ca28ca3a45a592bd1411229277c
nikhil-ydv <ynikhil95@gmail.com>
2022-06-08 11:00:32 +0530

    Wrote again Controller and Repository

---
+++ src/main/java/com/consultadd/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.consultadd.controller;
+
+import com.consultadd.model.Employee;
+import com.consultadd.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping("/employee")
+    public List<Employee> getEmployee() {
+        List<Employee> employeeList = employeeRepository.findAll();
+        return employeeList;
+
+    }
+
+}
